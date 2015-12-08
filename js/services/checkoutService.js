9a53bc2548b672800e2301996e877573c94cd137
jordangunther <jordankgunther@gmail.com>
2015-12-08 15:16:18 -0700

    Fixed line item SO not pushing to firebase

--- js/services/checkoutService.js
+++ js/services/checkoutService.js
@@ -1,7 +1,6 @@
 app.service('checkoutService', function($q){
 	var refSalesOrders = new Firebase('https://myallapp.firebaseio.com/revApp/salesOrders');
 	var refLineItems = new Firebase('https://myallapp.firebaseio.com/revApp/lineItems');
-	
 	this.getSalesOrder = function(){
 		var defer = $q.defer();
 		refSalesOrders.on('value', function(res){
@@ -21,31 +20,26 @@ app.service('checkoutService', function($q){
 		var lineItem = customerInfo;
 		delete lineItem.$$hashKey;
 		this.getSalesOrder().then(function(ref){
-			lineItem.salesOrder = ref + 1;	
-			refSalesOrders.push(lineItem.salesOrder);
-		})
-
-		if (orderItems.length) {
-			console.log(orderItems.length);
-			for (var i = 0; i < orderItems.length; i++) {
-				lineItem.product = orderItems[i].name;
-				lineItem.unitAmount = orderItems[i].amount;
-				lineItem.qty = orderItems[i].qty;
-				lineItem.extendedAmount = (orderItems[i].amount * orderItems[i].qty);
-				console.log(lineItem);
-				console.log(lineItem.salesOrder);
-				
+			var salesOrder = ref + 1;	
+			refSalesOrders.push(salesOrder);
+			if (orderItems.length) {
+				for (var i = 0; i < orderItems.length; i++) {
+					lineItem.product = orderItems[i].name;
+					lineItem.unitAmount = orderItems[i].amount;
+					lineItem.qty = orderItems[i].qty;
+					lineItem.extendedAmount = (orderItems[i].amount * orderItems[i].qty);
+					console.log('scope', salesOrder);
+					lineItem.salesOrder = salesOrder;
+					refLineItems.push(lineItem);
+				}
+			} else {
+				lineItem.product = orderItems.name;
+				lineItem.unitAmount = orderItems.amount;
+				lineItem.qty = orderItems.qty;
+				lineItem.extendedAmount = orderItems.extendedAmount;
+				lineItem.salesOrder = salesOrder;
 				refLineItems.push(lineItem);
 			}
-		} else {
-			lineItem.product = orderItems.name;
-			lineItem.unitAmount = orderItems.amount;
-			lineItem.qty = orderItems.qty;
-			lineItem.extendedAmount = orderItems.extendedAmount;
-			
-			refSalesOrders.push(lineItem.salesOrder);
-
-			refLineItems.push(lineItem);
-		}
+		})
 	}
 });
